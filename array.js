cad38f2427d01c7b754a0157d2f49d79c1679d8a
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-12-14 11:08:44 +0100

    includes()

--- array.js
+++ array.js
@@ -34,4 +34,77 @@ reduce()
 
 find()
 
+/*
+
 includes()
+
+const myArray = [1,2,3,4,5]
+undefined
+myArray.includes(3)
+true
+myArray.includes(9)
+false
+const newArray = [{id:1},{id:2},{id:3}]
+undefined
+myArray.includes(2,3)
+false
+myArray.includes(2,1)
+true
+newArray.includes({id:2})
+false
+var b = 3
+undefined
+var c = b
+undefined
+c
+3
+b = 5
+5
+c
+3
+const obj1 = {id:1}
+undefined
+const obj2 = {id:1}
+undefined
+obj1 === obj2
+false
+const obj3 = obj2
+undefined
+obj3
+Object { id: 1 }
+
+obj3 === obj2
+true
+obj2.id = 17
+17
+obj2
+Object { id: 17 }
+
+obj3
+Object { id: 17 }
+
+const o1 = {id:1}
+undefined
+const o2 ={id: 2}
+undefined
+const o3 = {id: 3}
+undefined
+const newestArray = [o1,o2,o3]
+undefined
+newestArray
+Array(3) [ {…}, {…}, {…} ]
+​
+0: Object { id: 1 }
+​
+1: Object { id: 2 }
+​
+2: Object { id: 3 }
+​
+length: 3
+​
+<prototype>: Array []
+
+newestArray.includes(o1)
+true
+
+*/
