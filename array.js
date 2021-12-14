769890a6a9c7d87a01191892fabb120ee41991e2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-12-14 09:56:30 +0100

    map()

--- array.js
+++ array.js
@@ -1,6 +1,20 @@
+/*
 const myArray = [1, 2, 3, 4]
 
+const myArray = [1,2,3,4]
+undefined
+myArray.map(el => el + 1)
+Array(4) [ 2, 3, 4, 5 ]
+
+myArray.map(() => "b")
+Array(4) [ "b", "b", "b", "b" ]
+
+myArray
+Array(4) [ 1, 2, 3, 4 ]
+
 map()
+
+*/
 
 filter()
 
